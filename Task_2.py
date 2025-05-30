0139aa9c7e9c16fb9f3525a578aebfc12fcc9345
yash0wiz <141996224+yash0wiz@users.noreply.github.com>
2025-05-30 23:23:11 +0530

    Create Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+Task 2: Using the Math Module for Calculations
+Problem Statement: Write a Python program that:
+1.   Asks the user for a number as input.
+2.   Uses the math module to calculate the:
+o   Square root of the number
+o   Natural logarithm (log base e) of the number
+o   Sine of the number (in radians)
+3.   Displays the calculated results.
+ 
