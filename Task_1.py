b908284731420ce6798e4222b4ddb5f326bfa3ae
yash0wiz <141996224+yash0wiz@users.noreply.github.com>
2025-05-30 23:35:22 +0530

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,6 +1,18 @@
-Task 1: Calculate Factorial Using a Function 
-Problem Statement: Write a Python program that:
-1.   Defines a function named factorial that takes a number as an argument and calculates its factorial using a loop or recursion.
-2.   Returns the calculated factorial.
-3.   Calls the function with a sample number and prints the output.
+#Task 1: Calculate Factorial Using a Function 
+#Problem Statement: Write a Python program that:
+#1.   Defines a function named factorial that takes a number as an argument and calculates its factorial using a loop or recursion.
+#2.   Returns the calculated factorial.
+#3.   Calls the function with a sample number and prints the output.
  
+
+def fact(num):
+    if num == 0:
+        return 1
+    else:
+        return num * fact(num - 1)
+
+num=int(input("Enter a number: "))
+result = fact(num)
+print("Factorial of  ",num," is : ", result)
+
+
